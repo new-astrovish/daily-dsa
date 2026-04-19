aa07265d868e453be232217b7ef480a9c4b4130e
new-astrovish <astrovish@gmail.com>
2026-04-20 00:05:22 +0530

    dsa: two sum

---
+++ twoSum.js
@@ -0,0 +1,57 @@
+/*
+Two Sum
+Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+Return the answer with the smaller index first.
+
+Example 1:
+
+Input: 
+nums = [3,4,5,6], target = 7
+
+Output: [0,1]
+Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+
+Example 2:
+
+Input: nums = [4,5,6], target = 10
+
+Output: [0,2]
+Example 3:
+
+Input: nums = [5,5], target = 10
+
+Output: [0,1]
+Constraints:
+
+2 <= nums.length <= 1000
+-10,000,000 <= nums[i] <= 10,000,000
+-10,000,000 <= target <= 10,000,000
+Only one valid answer exists.
+*/
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let resultObj = {};
+        let result = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+            result = target - nums[i];
+            if (resultObj.hasOwnProperty(result)) {
+                return [i, resultObj[result]];
+            }
+            resultObj[nums[i]] = i;
+        }
+    }
+}
+
+const nums = [3,4,5,6], target = 7;
+
+const solObj = new Solution();
+console.log(solObj.twoSum(nums, target));
